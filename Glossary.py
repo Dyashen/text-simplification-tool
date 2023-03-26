9ac610e785b5a48ad64a549bc3a899f3ae7c799a
Dylan Cluyse <dylan.cluyse@outlook.com>
2023-03-26 21:08:48 +0200

    gitignore add

--- Glossary.py
+++ Glossary.py
@@ -2,19 +2,6 @@ import pandoc, openai
 
 COMPLETIONS_MODEL = "text-davinci-003"
 
-def prompt_gpt(prompt, model, max_tokens, temperature):
-    # https://platform.openai.com/docs/api-reference/completions
-    return openai.Completion.create(
-            prompt=prompt,
-            temperature=temperature,
-            max_tokens=max_tokens,
-            model=model,
-            top_p=0.9,
-            stream=False
-        )["choices"][0]["text"].strip(" \n")
-
-
-
 def generate_glossary_for_set(set):
     words = ", ".join(set)
     prompt = f"""
@@ -23,17 +10,12 @@ def generate_glossary_for_set(set):
     woord: uitleg. synoniemen: ...\n
     """
 
-    result = prompt_gpt(
+    result = openai.Completion.create(
             prompt=prompt,
-            max_tokens=200,
-            model=COMPLETIONS_MODEL,
             temperature=0,
-            top_p=0.25
-    )
-    
-    print(result)
-
+            max_tokens=1000,
+            model=COMPLETIONS_MODEL,
+            top_p=0.9,
+            stream=False
+    )["choices"][0]["text"].strip(" \n")
     return result.split('\n')
-
-def generate_pdf():
-    return 'foo'
